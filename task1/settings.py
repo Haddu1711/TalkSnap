e53c3cc15f47512d303d12e53e885de6363aebe2
Haddu1711 <panchalhardik701@gmail.com>
2022-04-12 19:45:25 +0530

    change in settings.py

--- task1/settings.py
+++ task1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&0j!%dcc9gdzm2$i(sjria2xf%^r+yz=*f6-%2hj&!g6_h(w$!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
