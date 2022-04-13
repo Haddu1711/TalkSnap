0ca9a5bbe9882521ac68d494e2ce1b67a8eb2f82
Vinit <vinitm@nividata.com>
2022-04-13 11:18:51 +0530

    removed cache files, added static root

--- task1/settings.py
+++ task1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&0j!%dcc9gdzm2$i(sjria2xf%^r+yz=*f6-%2hj&!g6_h(w$!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 LOGIN_REDIRECT_URL = '/home/'
 MEDIA_ROOT = BASE_DIR / 'post'
 
